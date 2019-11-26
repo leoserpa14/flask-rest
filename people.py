c19411230c83a7a447dab8160396f72a0cf0138e
leoserpa14 <lscordeiro14@gmail.com>
2019-11-25 21:26:38 -0700

    Adding all CRUD functions to the API

--- people.py
+++ people.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from flask import make_response, abort
+
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -34,7 +36,7 @@ PEOPLE = {
 
 
 # Create a handler for our read (GET) people
-def read():
+def read_all():
     """
     This function responds to a request for /api/people
     with the complete lists of people
@@ -43,3 +45,30 @@ def read():
     """
     # Create the list of people from our data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+
+def read_one(lname):
+
+    # Does the person exists in our list?
+    if lname in PEOPLE: # Porque aqui é só 'in PEOPLE' e não 'in PEOPLE.keys'?
+        # person = PEOPLE[lname] # Isso não funciona
+        person = PEOPLE.get(lname)
+    else:
+        abort(
+            404, "Person with last name {lname} not found.".format(lname=lname)
+        )
+
+    return person
+
+    
+
+
+def create():
+    pass
+
+
+def update():
+    pass
+
+
+def delete():
+    pass
