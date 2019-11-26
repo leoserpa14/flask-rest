bb516bcbbf235f639dc59a1625a6a2d0d51ff6d2
leoserpa14 <lscordeiro14@gmail.com>
2019-11-26 01:04:48 -0700

    Finished basic CRUD Api

--- people.py
+++ people.py
@@ -40,35 +40,53 @@ def read_all():
     """
     This function responds to a request for /api/people
     with the complete lists of people
-
-    :return:        sorted list of people
     """
     # Create the list of people from our data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
 
-def read_one(lname):
 
+def create(person):
+    lname = person.get("lname", None)
+    fname = person.get("fname", None)
+    # Does the person exists already?
+    if lname not in PEOPLE and lname is not None:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp()
+        }
+        return make_response("{lname} successfully created.".format(lname=lname), 201)
+    else:
+        abort(406, "Person with last name {lname} already exists or is null".format(lname=lname))
+
+
+def read_one(lname):
     # Does the person exists in our list?
-    if lname in PEOPLE: # Porque aqui é só 'in PEOPLE' e não 'in PEOPLE.keys'?
+    if lname in PEOPLE: # Porque aqui é só 'in PEOPLE' e não 'in PEOPLE.keys'? - Porque PEOPLE é um dicionário de dicionários, e os dicionários já retornam as chaves
         # person = PEOPLE[lname] # Isso não funciona
         person = PEOPLE.get(lname)
     else:
         abort(
             404, "Person with last name {lname} not found.".format(lname=lname)
         )
-
-    return person
-
-    
+    return person   
 
 
-def create():
-    pass
+def update(lname, person):
+    # 'person' tem que receber um dicionário    
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname")
+        PEOPLE[lname]["lname"] = person.get("lname")
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(404, "Person with last name {lname} not found.".format(lname=lname))
 
 
-def update():
-    pass
-
-
-def delete():
-    pass
+def delete(lname):
+    # Does the person exists in our list?
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response("{lname} successfully deleted".format(lname=lname))
+    else:
+        abort(404, "Person with last name {lname} not found".format(lname=lname))
